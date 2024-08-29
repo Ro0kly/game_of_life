24cd38b54d36ff5b1a87c64bbd54caf33235bac7
Ro0kly <snallytango@yandex.ru>
2024-08-29 11:55:08 +0300

    main / complete game + add start_positions

--- src/main.c
+++ src/main.c
@@ -3,6 +3,33 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+void output(int n, int m, int field[n][m]);
+int top(int n, int m, int field_buffer[n][m], int i, int j);
+int check_friends(int n, int m, int field[n][m], int i, int j);
+void speed_change(int* p_speed);
+void init_ncurses();
+void fill_field(int n, int m, int (*field)[m]);
+
+void fill_field(int n, int m, int (*field)[m]) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            int x;
+            scanf("%d", &x);
+            field[i][j] = x;
+        }
+    }
+}
+
+void init_ncurses() {
+    if (freopen("/dev/tty", "r", stdin) != NULL) {
+        initscr();
+        timeout(1);
+        keypad(stdscr, TRUE);
+        noecho();
+        nodelay(stdscr, TRUE);
+    }
+}
+
 int check_friends(int n, int m, int field[n][m], int i, int j) {
     int friends = 0;
     for (int y = -1; y <= 1; y++) {
@@ -17,26 +44,6 @@ int check_friends(int n, int m, int field[n][m], int i, int j) {
     return friends;
 }
 
-void init_ncurses() {
-    if (freopen("/dev/tty", "r", stdin) != NULL) {
-        initscr();
-        timeout(1);
-        keypad(stdscr, TRUE);
-        noecho();
-        nodelay(stdscr, TRUE);
-    }
-}
-
-void fill_field(int n, int m, int (*field)[m]) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            int x;
-            scanf("%d", &x);
-            field[i][j] = x;
-        }
-    }
-}
-
 void output(int n, int m, int field[n][m]) {
     printw("\n");
     for (int i = 0; i < n; i++) {
@@ -46,7 +53,6 @@ void output(int n, int m, int field[n][m]) {
         printw("\n");
     }
 }
-
 void speed_change(int* p_speed) {
     int key = getch();
     if (key == 'w' && (*p_speed - 2500 > 0)) {
@@ -59,4 +65,41 @@ void speed_change(int* p_speed) {
     }
     if (key == 'q') *p_speed = 0;
     refresh();
+}
+
+int main() {
+    int n = 25, m = 80, speed = 30000;
+    int* p_speed = &speed;
+    int field[n][m], field_buffer[n][m];
+    fill_field(n, m, field);
+    init_ncurses();
+    while (*p_speed > 0) {
+        usleep(*p_speed);
+        clear();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                field_buffer[i][j] = field[i][j];
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int friends = check_friends(n, m, field, i, j);
+                if (friends != 2 && friends != 3) {
+                    field_buffer[i][j] = 0;
+                } else if (friends == 3) {
+                    field_buffer[i][j] = 1;
+                } else {
+                    field_buffer[i][j] = field[i][j];
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                field[i][j] = field_buffer[i][j];
+            }
+        }
+        output(n, m, field);
+        speed_change(p_speed);
+    }
+    endwin();
 }
